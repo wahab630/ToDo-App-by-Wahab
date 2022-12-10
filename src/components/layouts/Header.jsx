44a99b9a952e9b876e4f0add4eac2cad522bd1cc
wahab630 <wahabmalik630@gmail.com>
2022-12-10 18:35:40 +0500

    added

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -5,7 +5,7 @@ import { Link } from "react-router-dom";
 const Header = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg  sticky-top">
+      <nav className="navbar navbar-expand-lg  ">
         <div className="container">
           <Link className="navbar-brand" to="/">
             Todo
